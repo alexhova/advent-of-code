0c19b3fb2a0cf4360d77bccafc9ac410641bacdd
alexhova <alexhovancik@gmail.com>
2021-12-02 13:00:16 -0600

    day 2 complete

---
+++ 2021/02/part1.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+    position = 0
+    depth = 0
+
+    with open("commands.txt") as f:
+        for command in f:
+            direction, magnitude = command.split(" ")
+            magnitude = int(magnitude)
+
+            if direction == "down":
+                depth += magnitude
+            elif direction == "up":
+                depth -= magnitude    
+            elif direction == "forward":
+                position += magnitude
+
+    print(f"final position is {position*depth}")
