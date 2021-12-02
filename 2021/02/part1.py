c33afd8f93069414d1605d484b412ab9c97f6f9e
alexhova <alexhovancik@gmail.com>
2021-12-02 13:41:08 -0600

    remove main function because it the only function

--- 2021/02/part1.py
+++ 2021/02/part1.py
@@ -1,17 +1,16 @@
-if __name__ == "__main__":
-    position = 0
-    depth = 0
+position = 0
+depth = 0
 
-    with open("commands.txt") as f:
-        for command in f:
-            direction, magnitude = command.split(" ")
-            magnitude = int(magnitude)
+with open("commands.txt") as f:
+    for command in f:
+        direction, magnitude = command.split(" ")
+        magnitude = int(magnitude)
 
-            if direction == "down":
-                depth += magnitude
-            elif direction == "up":
-                depth -= magnitude    
-            elif direction == "forward":
-                position += magnitude
+        if direction == "down":
+            depth += magnitude
+        elif direction == "up":
+            depth -= magnitude
+        elif direction == "forward":
+            position += magnitude
 
-    print(f"final position is {position*depth}")
+print(f"final position is {position*depth}")
--- 2021/02/part2.py
+++ 2021/02/part2.py
@@ -1,19 +1,18 @@
-if __name__ == "__main__":
-    position = 0
-    depth = 0
-    aim = 0
+position = 0
+depth = 0
+aim = 0
 
-    with open("commands.txt") as f:
-        for command in f:
-            direction, magnitude = command.split(" ")
-            magnitude = int(magnitude)
+with open("commands.txt") as f:
+    for command in f:
+        direction, magnitude = command.split(" ")
+        magnitude = int(magnitude)
 
-            if direction == "down":
-                aim += magnitude
-            elif direction == "up":
-                aim -= magnitude    
-            elif direction == "forward":
-                position += magnitude
-                depth += aim * magnitude
+        if direction == "down":
+            aim += magnitude
+        elif direction == "up":
+            aim -= magnitude
+        elif direction == "forward":
+            position += magnitude
+            depth += aim * magnitude
 
-    print(f"final position is {position*depth}")
+print(f"final position is {position*depth}")
