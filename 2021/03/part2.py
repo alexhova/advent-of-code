10634c1e344642ad598d40964a34fd4bd0c19ae1
alexhova <alexhovancik@gmail.com>
2021-12-03 16:09:33 -0600

    simplify terminating condition

--- 2021/03/part2.py
+++ 2021/03/part2.py
@@ -16,7 +16,7 @@ gamma, epsilon = calculate_rates(totals, count)
 oxys = [line.strip() for line in lines if line.startswith(gamma[0])]
 co2s = [line.strip() for line in lines if line.startswith(epsilon[0])]
 i = 1
-while i < length or not (len(oxys) == len(co2s) == 1):
+while not (len(oxys) == len(co2s) == 1):
     oxys = [oxy for oxy in oxys if oxy[i] == mode(oxys, i)] if len(oxys) > 1 else oxys
     co2s = [co2 for co2 in co2s if co2[i] != mode(co2s, i)] if len(co2s) > 1 else co2s
     i += 1
