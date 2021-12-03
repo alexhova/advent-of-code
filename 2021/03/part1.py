8d44adb4398213793131defbc760ed495879f511
alexhova <alexhovancik@gmail.com>
2021-12-03 13:29:49 -0600

    complete day3 part1

---
+++ 2021/03/part1.py
@@ -0,0 +1,34 @@
+def read_diagnostics():
+    with open("diagnostics.txt") as f:
+        lines = f.readlines()
+    length = len(lines[0].strip())
+    totals = [0] * length
+    count = len(lines)
+    for line in lines:
+        totals = [totals[i] + int(line[i].strip()) for i in range(length)]
+    return totals, count
+
+
+def calculate_rates(totals, count):
+    gamma = ""
+    epsilon = ""
+    for ones in totals:
+        zeroes = count - ones
+        if ones > zeroes:
+            gamma += "1"
+            epsilon += "0"
+        elif zeroes > ones:
+            gamma += "0"
+            epsilon += "1"
+    return gamma, epsilon
+
+
+def power_consumption(gamma, epsilon):
+    return int(gamma, 2) * int(epsilon, 2)
+
+
+if __name__ == "__main__":
+    totals, count = read_diagnostics()
+    gamma, epsilon = calculate_rates(totals, count)
+    power = power_consumption(gamma, epsilon)
+    print(f"power consumption is {power}")
